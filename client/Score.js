fd3fb388336a4cc33d38b98c6c79137a5e494512
Kris Burke <kris.burke611@gmail.com>
2016-01-23 18:10:54 -0500

    Added collision detection and score incrementing

---
+++ client/Score.js
@@ -0,0 +1,12 @@
+var Score = function (text, value) {
+  this.text = text;
+  this.value = value;
+}
+
+Score.prototype.scoreFactory = function () {
+  var highScore = new Score("High Score: ", 0);
+  var currScore = new Score("Current Score: ", 0);
+  var collisions = new Score("Collisions: ", 0);
+  
+  return [highScore, currScore, collisions];
+};
--- client/watchout.js
+++ client/watchout.js
@@ -2,6 +2,7 @@
 var spec = { "boardHeight":500,
              "boardWidth": 500,
              "enemyRadius": 50,
+             "playerRadius": 20,
              "interval": 1000,
              "numberOfEnemies": 10,
            };
@@ -10,36 +11,75 @@ var spec = { "boardHeight":500,
 var board = d3.select("body").append("svg")
   .attr('height', spec.boardHeight)
   .attr('width', spec.boardWidth);
-
 var enemies = Dot.prototype.enemyFactory(spec.numberOfEnemies);
 var player = new Dot(spec.boardWidth/2, spec.boardHeight/2);
+
+var scoreboard = d3.select(".scoreboard");
+var scores = Score.prototype.scoreFactory();
 
 function update(enemies) {
 
   //JOIN
   var enemyData = board.selectAll(".enemy").data(enemies);
+  var scoreData = scoreboard.selectAll("div").data(scores)
+                    .text(function (d, i) {
+                      if (i === 1) {
+                        d.value++;
+                      }
+                      return d.text + d.value});
+  
+  
 
   //ENTER
   enemyData.enter().append("circle")
     .attr("class", "enemy")
-    .attr("cx", function (d){ return d.x; })
-    .attr("cy", function (d){ return d.y; });
+    .attr("cx", function (d, i){ return d.x; })
+    .attr("cy", function (d, i){ return d.y; });
+    
 
   //UPDATE
-  enemyData.transition().attr({cx: function(d){ d.x = d.randomCoord(); return d.x; },
-                             cy: function(d){ d.y = d.randomCoord(); return d.y; } });  
+  enemyData.transition()
+    .tween('track-position', function (d) {   
+      d.x = this.cx.baseVal.value;
+      d.y = this.cy.baseVal.value;
+      
+      var distance = Math.sqrt(Math.pow((d.x-player.x), 2) + Math.pow((d.y-player.y), 2));
+      
+      if (distance < spec.enemyRadius + spec.playerRadius) {
+        scores[2].value++;
+        if (scores[1].value > scores[0].value) {
+          scores[0].value = scores[1].value;
+        }
+        scores[1].value = 0;
+      }
+    })
+    .attr({cx: function(d){ return d.randomCoord(); },
+           cy: function(d){ return d.randomCoord(); } 
+    });  
 
   //EXIT
   enemyData.exit().remove();
 }
 
+// JOIN player
 var playerData = board.selectAll(".player").data([player]);
 
+// DRAG player
 var drag = d3.behavior.drag()
-  .on("drag", function() { playerData.attr("cx", d3.event.x) 
-                                     .attr("cy", d3.event.y)
+  .on("drag", function() {
+    // Check if in bounds before moving piece 
+    if (d3.event.x < spec.boardWidth && d3.event.y < spec.boardHeight
+        && d3.event.x > 0 && d3.event.y > 0) {
+      // Move the player piece
+      playerData.attr("cx", d3.event.x) 
+        .attr("cy", d3.event.y);
+      // Update player data
+      player.x = d3.event.x;
+      player.y = d3.event.y;
+      }
   });
 
+// ENTER player
 playerData.enter().append("circle")
   .attr("cx", function (d){ return d.x; })
   .attr("cy", function (d){ return d.y; })
@@ -48,4 +88,5 @@ playerData.enter().append("circle")
   
 setInterval(function(){
   update(enemies);
+
 }, spec.interval);
